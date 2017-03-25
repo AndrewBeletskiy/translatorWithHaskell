f83fbfb4d9c1937ecd20a9852af0f2333f57de51
Андрій Білецький < >
2017-03-25 14:11:41 +0200

    It recognize keywords and identifiers.

--- LexicalAnalyzer.hs
+++ LexicalAnalyzer.hs
@@ -6,38 +6,52 @@ import Data.Maybe
 import Types
 
 lexicalAnalyzer :: String -> Either [Lexeme] String
-lexicalAnalyzer text = f (convertToMyString text) 1 [] []
-    where 
-        f [] state curStr res
-            | state < 0 = Right curStr
-            | state == 1 = Left (reverse res)
-            | otherwise = Right ("Error! Unexpected end of file. Current string: '"++ curStr ++ "'")
-        f code@(x:xs) state curStr res
-            | state < 0 = Right curStr
-            | otherwise = let nextStateStrRes = changeState x state curStr res  
-                          in f xs (fst' nextStateStrRes) (snd' nextStateStrRes) (thrd' nextStateStrRes)
-        changeState chr@(MyChar val pos) state curStr res
-            | state == 1 = case cls of CharClassLetter ->    (2, val:curStr, res)
-                                       CharClassDigit ->     (3, val:curStr, res)
-                                       CharClassPoint ->     (4, val:curStr, res)
-                                       CharClassQuote ->     (9, val:curStr, res)
-                                       CharClassDelimiter -> (1, [], (Lexeme [val] (findLexemCodeOrID [val]) pos):res)
-                                       CharClassColon ->     (10, val:curStr, res)
-                                       CharClassEqual ->     (1, [], (Lexeme [val] EQUAL pos):res)
-                                       CharClassMore ->      (11, val:curStr, res)
-                                       CharClassLess ->      (12, val:curStr, res)
-                                       CharClassExclamationPoint -> (13, val:curStr, res)
-                                       CharClassEmpty ->     (1, [], res)
-                                       otherwise ->          (-1, "Illegal character '"++[val]++"' on " ++ (show pos), [])
-            | state == 2 = case cls of CharClassLetter -> (2, val:curStr, res)
-                                       CharClassDigit -> (2, val:curStr, res)
-                                       otherwise -> (changeState chr 1 [] $ (Lexeme (reverse curStr) (findLexemCodeOrID $ reverse curStr) pos):res)
-            | cls == CharClassError = (-1, "Illegal character '"++[val]++"' on " ++ (show pos), [])
-            | otherwise = (-1, "There is no such situation in automat!"++(show state)++" "++(show chr), [])
-            where cls = getCharClass chr
-        thrd' (_,_,x) = x
-        snd' (_,x,_) = x
-        fst' (x,_,_) = x
+lexicalAnalyzer text = f (convertToMyString $ text++" ") 1 [] []
+  where
+    f [] state curStr res
+        | state < 0 = Right curStr
+        | state == 1 = Left (reverse res)
+        | otherwise = Right ("Error! Unexpected end of file. S: "
+                             ++(show state)
+                             ++". Current string: '"
+                             ++ curStr ++ "'")
+    f code@(x:xs) state curStr res
+        | state < 0 = Right curStr
+        | otherwise = 
+            let nextStateStrRes = changeState x state curStr res
+            in f xs (fst' nextStateStrRes) 
+                    (snd' nextStateStrRes) 
+                    (thrd' nextStateStrRes)
+    changeState chr@(MyChar val pos) state curStr res
+        | state == 1 = case cls of 
+          CharClassLetter ->    (2, val:curStr, res)
+          CharClassDigit ->     (3, val:curStr, res)
+          CharClassPoint ->     (4, val:curStr, res)
+          CharClassQuote ->     (9, val:curStr, res)
+          CharClassDelimiter -> (1, [], (Lexeme [val] (findLexemCodeOrID [val]) pos):res)
+          CharClassColon ->     (10, val:curStr, res)
+          CharClassEqual ->     (1, [], (Lexeme [val] EQUAL pos):res)
+          CharClassMore ->      (11, val:curStr, res)
+          CharClassLess ->      (12, val:curStr, res)
+          CharClassExclamationPoint -> (13, val:curStr, res)
+          CharClassEmpty ->     (1, [], res)
+          otherwise ->          (-1, "Illegal character '"
+                                     ++[val]++"' on " 
+                                     ++ (show pos), [])
+        | state == 2 = case cls of CharClassLetter -> (2, val:curStr, res)
+                                   CharClassDigit -> (2, val:curStr, res)
+                                   otherwise -> (changeState chr 1 [] 
+                                                 $ (Lexeme (reverse curStr) 
+                                                           (findLexemCodeOrID $ reverse curStr) 
+                                                           pos):res)
+        | cls == CharClassError = (-1, "Illegal character '"
+                                       ++[val]++"' on " 
+                                       ++ (show pos), [])
+        | otherwise = (-1, "There is no such situation in automat!"++(show state)++" "++(show chr), [])
+        where cls = getCharClass chr
+    thrd' (_,_,x) = x
+    snd' (_,x,_) = x
+    fst' (x,_,_) = x
 
 findLexemCodeOrID :: [Char] -> LexemeCode
 findLexemCodeOrID str = fromMaybe ID $ lookup str strLexemeCodes
